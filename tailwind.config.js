3bc5cd352d8fc5221755fcbb22e39f38f338491a
Luis De Freites <luis.defreites@intelygenz.com>
2024-12-17 21:14:10 +0100

    feat: Add shadcn

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,13 @@
 const colors = require("tailwindcss/colors");
 
 export default {
+  darkMode: ["class"],
   content: [
     "./index.html",
     "./src/*.{js,ts,jsx,tsx}",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    colors: {
-      "moderate-cyan": "#3cb4ac",
-      "dark-cyan": "#147b74",
-      "dark-gray": "#7a7a7a",
-      black: colors.black,
-      white: colors.white,
-    },
     extend: {
       backgroundImage: {
         "hero-mobile": "url('/src/assets/image-hero-mobile.jpg')",
@@ -23,7 +17,57 @@ export default {
       screens: {
         xs: "350px",
       },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {
+        "moderate-cyan": "#3cb4ac",
+        "dark-cyan": "#147b74",
+        "dark-gray": "#7a7a7a",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          1: "hsl(var(--chart-1))",
+          2: "hsl(var(--chart-2))",
+          3: "hsl(var(--chart-3))",
+          4: "hsl(var(--chart-4))",
+          5: "hsl(var(--chart-5))",
+        },
+      },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
