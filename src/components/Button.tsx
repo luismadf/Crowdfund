3da18a14b291056f583afcdfce2d29295212fd1b
Luis De Freites <luis.defreites@intelygenz.com>
2024-12-17 20:58:53 +0100

    bugfix: Fix Button props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,12 @@
 import { ButtonHTMLAttributes } from "react";
 
 export default function Button(props: ButtonHTMLAttributes<HTMLButtonElement>) {
-  const { children, disabled } = props;
+  const { children, disabled, ...otherProps } = props;
   return (
     <button
       disabled={disabled}
-      className="bg-moderate-cyan hover:bg-dark-cyan md:hover:bg-dark-cyan disabled:hover:bg-dark-gray disabled:bg-dark-gray xs:px-10 rounded-full px-4 py-5 font-bold leading-4 tracking-wide text-white transition duration-150 ease-in"
+      className="rounded-full bg-moderate-cyan px-4 py-5 font-bold leading-4 tracking-wide text-white transition duration-150 ease-in disabled:bg-dark-gray disabled:hover:bg-dark-gray xs:px-10 lg:hover:bg-dark-cyan"
+      {...otherProps}
     >
       {children}
     </button>
