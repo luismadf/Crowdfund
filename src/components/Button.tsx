44c02eace2ca7095b56c6960db0d8e8556943210
Luis De Freites <luis.defreites@intelygenz.com>
2025-01-03 12:30:20 +0100

    feat: Make the Button able to take classes

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,15 @@
 import { ButtonHTMLAttributes } from "react";
+import { twMerge } from "tailwind-merge";
 
 export default function Button(props: ButtonHTMLAttributes<HTMLButtonElement>) {
-  const { children, disabled, ...otherProps } = props;
+  const { children, disabled, className, ...otherProps } = props;
   return (
     <button
       disabled={disabled}
-      className="rounded-full bg-moderate-cyan px-4 py-5 font-bold leading-4 tracking-wide text-white transition duration-150 ease-in disabled:bg-dark-gray disabled:hover:bg-dark-gray xs:px-10 lg:hover:bg-dark-cyan"
+      className={twMerge(
+        "rounded-full bg-moderate-cyan px-4 py-5 font-bold leading-4 tracking-wide text-white transition duration-150 ease-in disabled:bg-dark-gray disabled:hover:bg-dark-gray xs:px-10 lg:hover:bg-dark-cyan",
+        className,
+      )}
       {...otherProps}
     >
       {children}
