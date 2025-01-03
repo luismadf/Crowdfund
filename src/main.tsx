337869ea18d97481388111b37a17b2bc7679f833
Luis De Freites <luis.defreites@intelygenz.com>
2025-01-03 13:43:03 +0100

    feat: Install msw

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,21 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { bool } from "./constants/index.ts";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+async function enableMocking() {
+  if (import.meta.env.VITE_ENABLE_MOCK_API === bool.TRUE) {
+    const { worker } = await import("@/mocks/browser.ts");
+    return worker.start();
+  } else {
+    Promise.resolve();
+  }
+}
+
+enableMocking().then(() => {
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+});
