7c01c7c506ac315006fa52d72fd07c4d42f8e99a
Luis De Freites <luis.defreites@intelygenz.com>
2025-01-14 19:23:29 +0100

    feat: Add the format prop to StatsDisplay component

--- src/components/StatsDisplay.tsx
+++ src/components/StatsDisplay.tsx
@@ -1,13 +1,26 @@
+import { formatToLocalNumber, formatToUSD } from "@/lib/utils";
+
 export default function StatsDisplay(props: {
-  value: string;
+  type?: "number" | "currency";
+  value: string | number;
   description: string;
 }) {
-  const { value, description } = props;
+  const { type, value, description } = props;
+
+  let formattedValue = value;
+
+  if (type === "number") {
+    formattedValue = formatToLocalNumber(Number(value));
+  }
+
+  if (type === "currency") {
+    formattedValue = formatToUSD(Number(value));
+  }
 
   return (
     <div className="flex flex-col items-center md:items-start">
-      <span className="mb-2 text-[32px] font-bold">{value}</span>
-      <span className="text-dark-gray font-sm tracking-wide">
+      <span className="mb-2 text-[32px] font-bold">{formattedValue}</span>
+      <span className="font-sm tracking-wide text-dark-gray">
         {description}
       </span>
     </div>
