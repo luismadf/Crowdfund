4651d7d917dfcd09f3f6cfe2340ab3f924df6973
Luis De Freites <luis.defreites@intelygenz.com>
2025-01-14 19:25:19 +0100

    fix: Change ProgressBar component to accept number values instead of strings

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,8 +1,8 @@
-export default function ProgressBar(props: { value: string }) {
+export default function ProgressBar(props: { value: number }) {
   const { value } = props;
   return (
     <progress
-      className="[&::-webkit-progress-value]:bg-moderate-cyan h-3 w-full [&::-moz-progress-bar]:bg-[#F4F4F4] [&::-webkit-progress-bar]:rounded-full [&::-webkit-progress-bar]:bg-[#F4F4F4] [&::-webkit-progress-value]:rounded-full"
+      className="h-3 w-full [&::-moz-progress-bar]:bg-[#F4F4F4] [&::-webkit-progress-bar]:rounded-full [&::-webkit-progress-bar]:bg-[#F4F4F4] [&::-webkit-progress-value]:rounded-full [&::-webkit-progress-value]:bg-moderate-cyan"
       max="100"
       value={value}
     />
